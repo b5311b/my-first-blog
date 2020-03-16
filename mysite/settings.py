848097d44015fe5b094b680c45af8997d89be406
bb <btbognar@gmail.com>
2020-03-16 14:15:38 +0100

    Added the commemt option.

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
